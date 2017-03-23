8c933d7d211d885eef2bca3999d4eebeea72557c
Matthew Tabor <artist.cutter@gmail.com>
2017-03-23 18:16:55 +0100

    Added new excercise
    Added files required in lesson 8.3

---
+++ 8.3/js/scripts.js
@@ -0,0 +1,5 @@
+var a = prompt('wpisz długość boku');
+var h = prompt('wpisz wysokość');
+var triangleArea = a*h/2;
+
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
