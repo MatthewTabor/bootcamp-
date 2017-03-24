328f690fd58f99fe79e6b5126793af0994350cdc
Matthew Tabor <artist.cutter@gmail.com>
2017-03-24 21:03:54 +0100

    8.5 task
    Next task file changed

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,23 @@
-var name = prompt('Wpisz imię');
-alert('Nie ma tu nic ciekawego do zobaczenia, ' + name); //Brakuje komentarzy!
-console.log('Nie ma tu nic ciekawego do zobaczenia, ' + name);
+var a = prompt('Wprowadź liczbę a'),
+    h = prompt('Wprowadź liczbę h');
+
+function getTriangleArea(a, h) {
+	
+	if(a >= 0 && h >= 0) {
+		
+		return(a*h/2);
+			
+	  } else {
+
+		console.log('Nieprawidłowe dane');
+		
+	  }
+  }
+
+console.log('Pole trójkąta dla wprowadzonych liczb wynosi: ' + getTriangleArea(a,h));
+
+var triangleArea1 = getTriangleArea(10,15);
+
+    triangleArea2 = getTriangleArea(10,6);
+
+    triangleArea3 = getTriangleArea(8, 10);
