c80e7638577135e462a3df4ea7d5d857a100de9d
Matthew Tabor <artist.cutter@gmail.com>
2017-04-08 01:34:03 +0200

    indicators
    working on indicators

--- Carousel/js/query.js
+++ Carousel/js/query.js
@@ -7,20 +7,24 @@ var interval = setInterval(slideImg, 3000);
 $('.rightBtn').click(function(){
 	slideImg();
 	clearInterval(interval);
+	activeIndicator();
 });
 
 $('.leftBtn').click(function(){
 	slideImgBack();
 	clearInterval(interval);
+	reverseIndicator();
 });
 
 $('.resetInt').click(function(){
 	setInterval(slideImg, 3000);
+	activeIndicator();
 });
 
 function slideImg() {
 
 	carouselList.animate({'marginLeft': -400}, 700, moveFirstSlide);
+	activeIndicator();
 }
 
 function slideImgBack() {
@@ -45,6 +49,33 @@ function moveLastSlide() {
 
 	firstItem.before(lastItem);
 	carouselList.css({"marginLeft": -400});
+}
+
+var indicateList = $('#indicate');
+
+function activeIndicator() {
+
+	var firstItem = indicateList.find("li:first");
+	var lastItem = indicateList.find("li:last");
+
+	$(".active").next().addClass("active");
+	$(".active").prev().removeClass("active");
+
+	lastItem.after(function(){
+		$("li").first().addClass("active");
+		$("li").last().removeClass("active");
+	});
+
+}
+
+function reverseIndicator() {
+
+	var firstItem = indicateList.find("li:first");
+	var lastItem = indicateList.find("li:last");
+
+	$(".active").prev().addClass("active");
+	$(".active").next().removeClass("active");
+
 }
 
 });
