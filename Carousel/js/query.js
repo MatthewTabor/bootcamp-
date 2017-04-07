dd3e31dbf64b4b9857952e5f48f9132cb98aaee5
Matthew Tabor <artist.cutter@gmail.com>
2017-04-07 23:48:30 +0200

    FInished
    Finished carousel. No ambition :c

---
+++ Carousel/js/query.js
@@ -0,0 +1,23 @@
+$(function(){
+
+	var carouselList = $("#carousel ul");
+
+setInterval(slideImg, 2000);
+
+function slideImg() {
+
+	carouselList.animate({'marginLeft': -400}, 700, moveFirstSlide);
+}
+
+function moveFirstSlide() {
+
+	var firstItem = carouselList.find("li:first");
+	var lastItem = carouselList.find("li:last");
+
+	lastItem.after(firstItem);
+	carouselList.css({"marginLeft":0});
+}
+
+});
+
+
